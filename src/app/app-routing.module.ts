87cde1ad9087ed402bf955a743dca9a5afa36fc4
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-12 11:56:05 +0100

    Routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+// dominio.com/''
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full' // Aixo es per assegurar que el path sigui exactament com el que posem => el buit. Si no ho posem, tots els paths poden ser ''
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
@@ -7,9 +6,7 @@ import { Error404PageComponent } from './pages/error404-page/error404-page.compo
   declarations: [
     Error404PageComponent
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [],
   exports: [
     Error404PageComponent
   ]
